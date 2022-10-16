86a23a7ce1d4edb2b26d2f093ad28a32954fe19b
LidaCorrales <lkcorrales99@misena.edu.co>
2022-10-15 22:35:07 -0500

    Primer report

---
+++ Ejercicios/Conversion_temperatura.py
@@ -0,0 +1,10 @@
+C = int(input("Ingrese su valor en grados celsius")) #C = Celsius
+
+F = C * 1.8 + 32      #Formula de C a Fahrenheit
+print("El valor", C, "en grados Fahrenheit es: ", F)
+
+K = C + 273.15        #Formula para convertir C a Kelvin
+print("El valor", C, "en grados Kelvin es", K)
+
+R = C * 9/5 + 491.67  #Formula de Rankine 
+print("El valor", C, "en grados Rankine es", R)
