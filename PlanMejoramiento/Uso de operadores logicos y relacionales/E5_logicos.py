0e1890588c3474ea28e1bd7391477c0481e2c497
LidaCorrales <lkcorrales99@misena.edu.co>
2022-11-24 20:09:48 -0500

    Repositorio PlanMejora 2

--- PlanMejoramiento/Uso de operadores logicos y relacionales/E5_logicos.py
+++ PlanMejoramiento/Uso de operadores logicos y relacionales/E5_logicos.py
@@ -7,3 +7,12 @@ numero = int(input("Introducir numero: "))
 
 resultado = numero // 2
 print(resultado)
+
+#Operador logico
+if resultado:
+    numero % 2 == 0 or numero % 3 == 0
+    print("el numero es divisible por 2 o 3")
+else:
+    print("Numero es 0")
+"""es True si una de las dos condiciones es True, esto es, si el número n es divisible por 2 o es
+divisible por 3"""
